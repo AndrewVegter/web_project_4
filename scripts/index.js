2a29ab8d4138257ef61e46204965f322088d7bd9
Andrew Vegter <andrew.r.vegter@gmail.com>
2022-02-17 17:37:38 -0600

    bugfix: fixed js errors

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,21 @@ const exitBio = document.querySelector("#exit-bio");
 const exitAdd = document.querySelector("#exit-add");
 const exitImage = document.querySelector("#exit-image");
 const addButton = document.querySelector(".profile__add-button");
-const popout = document.querySelector(".popout");
 const formBio = document.querySelector("#edit-container");
-const popoutEdit = document.querySelector("#edit-bio");
+const popupEdit = document.querySelector("#edit-bio");
 const formName = document.querySelector("#name");
 const formAbout = document.querySelector("#about-me");
 const formAdd = document.querySelector("#add-container");
-const popoutAdd = document.querySelector("#add-image");
+const popupAdd = document.querySelector("#add-image");
 const formTitle = document.querySelector("#title");
 const formLink = document.querySelector("#image");
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__career");
 const cardContainer = document.querySelector(".gallery__container");
-const popoutImage = document.querySelector(".popout__image");
-const popoutImageTitle = document.querySelector(".popout__image-title");
-const popoutImageVeil = document.querySelector("#image-container");
+const popupImage = document.querySelector(".popout__image");
+const popupImageTitle = document.querySelector(".popout__image-title");
+const popupImageVeil = document.querySelector("#image-container");
+const cardTemplate = document.querySelector("#card-template").content;
 const initialCards = [
     {
       name: "Yosemite Valley",
@@ -45,8 +45,7 @@ const initialCards = [
     }
   ];
 
-function addCard(cardName, cardLink) {
-    const cardTemplate = document.querySelector("#card-template").content;
+function renderCard(cardName, cardLink) {
     const cardAdded = cardTemplate.querySelector(".gallery__cell").cloneNode(true);
     const cardTitle = cardAdded.querySelector(".gallery__title");
     const cardImage = cardAdded.querySelector(".gallery__image");
@@ -56,42 +55,50 @@ function addCard(cardName, cardLink) {
     cardImage.alt = `A picture of ${cardName}`;
     cardImage.src = cardLink; 
     cardImage.addEventListener("click", () => {
-      popoutImage.src = cardImage.src;
-      popoutImage.alt = cardImage.alt;
-      popoutImageTitle.textContent = cardTitle.textContent;
-      popoutImageVeil.style.opacity = 1;
-      popoutImageVeil.style.visibility = "visible";
-    })
+      popupImage.src = cardImage.src;
+      popupImage.alt = cardImage.alt;
+      popupImageTitle.textContent = cardTitle.textContent;
+      togglePopup(popupImageVeil);
+    });
     cardDelete.addEventListener("click", () => {
         cardAdded.remove();
     });
     cardLike.addEventListener("click", () => {
         cardLike.classList.toggle("gallery__button_active");
     });
-    cardContainer.prepend(cardAdded);
+    return cardAdded;
+}
+
+function createCard(name, link) {
+  const card = renderCard(name, link);
+  cardContainer.prepend(card);
 }
 
 initialCards.forEach(item => {
-    addCard(item.name, item.link);
+    createCard(item.name, item.link);
     return;
 });
 
-function exitPopout(evt) {
+function togglePopup(container) {
+  container.classList.toggle("popout__invisible");
+}
+
+function exitPopup(evt) {
     const targ = evt.currentTarget;
     const container = targ.parentNode.parentNode;
-    formName.value = "";
-    formAbout.value = "";
-    formTitle.value = "";
-    formLink.value = "";
-    container.style.opacity = 0;
-    container.style.visibility = "hidden";
+    togglePopup(container);
+}
+
+function resetAdd(evt) {
+  popupAdd.reset();
+  exitPopup(evt);
 }
 
 function submitBio(evt) {
     evt.preventDefault();
     profileName.textContent = formName.value;
     profileAbout.textContent = formAbout.value;
-    exitPopout(evt);
+    exitPopup(evt);
 }
 
 editButton.addEventListener("click", () => {
@@ -99,25 +106,23 @@ editButton.addEventListener("click", () => {
     const aboutValue = profileAbout.textContent;
     formName.value = nameValue;
     formAbout.value = aboutValue;
-    formBio.style.opacity = 1;
-    formBio.style.visibility = "visible";
+    togglePopup(formBio);
 });
 
 addButton.addEventListener("click", () => {
-    formAdd.style.opacity = 1;
-    formAdd.style.visibility = "visible";
+    togglePopup(formAdd);
 })
 
-exitBio.addEventListener("click", exitPopout);
+exitBio.addEventListener("click", exitPopup);
 
-exitAdd.addEventListener("click", exitPopout);
+exitAdd.addEventListener("click", resetAdd);
 
-exitImage.addEventListener("click", exitPopout);
+exitImage.addEventListener("click", exitPopup);
 
-popoutEdit.addEventListener("submit", submitBio);
+popupEdit.addEventListener("submit", submitBio);
 
-popoutAdd.addEventListener("submit", (evt) => {
+popupAdd.addEventListener("submit", (evt) => {
     evt.preventDefault();
-    addCard(`${formTitle.value}`, formLink.value)
-    exitPopout(evt);
+    createCard(`${formTitle.value}`, formLink.value)
+    exitPopup(evt);
 });
